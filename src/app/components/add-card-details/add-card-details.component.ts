144c5ca6b9fa8d9db3ff6e3e40084740ead12c76
Tina Eide <eide.tin@gmail.com>
2025-04-22 18:06:36 +0200

    Add components add and details

---
+++ src/app/components/add-card-details/add-card-details.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-add-card-details',
+  templateUrl: './add-card-details.component.html'
+})
+
+export class AddCardDetailsComponent {
+
+}
