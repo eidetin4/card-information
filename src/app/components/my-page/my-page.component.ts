01753f1cb4750eddb45309bf289e50182822ad23
Tina Eide <eide.tin@gmail.com>
2025-04-22 17:00:39 +0200

    Get and display all cards

--- src/app/components/my-page/my-page.component.ts
+++ src/app/components/my-page/my-page.component.ts
@@ -1,10 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {CardDetailsService} from '../../services/card-details.service';
+import {GetCardDetails} from '../../models/card-details-models';
 
 @Component({
   selector: 'app-my-page',
   templateUrl: './my-page.component.html'
 })
 
-export class MyPageComponent {
+export class MyPageComponent implements OnInit {
 
+  private cardInformationService: CardDetailsService = inject(CardDetailsService);
+
+  protected cards: GetCardDetails[] = [];
+
+  ngOnInit(): void {
+    this.cardInformationService.getAllCardDetails().subscribe({
+      next: (response: GetCardDetails[]): void => {
+        this.cards = response;
+      },
+      error: (error: Error): void => {
+        console.error('Error fetching card information:', error.message);
+      }
+    });
+  }
 }
