90ea93ad1ab212bb647a0d264acf291956bbb4a0
Tina Eide <eide.tin@gmail.com>
2025-04-22 18:00:50 +0200

    Convert to signals

--- src/app/components/my-page/my-page.component.ts
+++ src/app/components/my-page/my-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, signal, WritableSignal} from '@angular/core';
 import {CardDetailsService} from '../../services/card-details.service';
 import {GetCardDetails} from '../../models/card-details-models';
 
@@ -11,12 +11,12 @@ export class MyPageComponent implements OnInit {
 
   private cardInformationService: CardDetailsService = inject(CardDetailsService);
 
-  protected cards: GetCardDetails[] = [];
+  protected readonly cards: WritableSignal<GetCardDetails[]> = signal<GetCardDetails[]>([]);
 
   ngOnInit(): void {
     this.cardInformationService.getAllCardDetails().subscribe({
       next: (response: GetCardDetails[]): void => {
-        this.cards = response;
+        this.cards.set(response);
       },
       error: (error: Error): void => {
         console.error('Error fetching card information:', error.message);
