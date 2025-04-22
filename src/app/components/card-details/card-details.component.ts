e3deb3acb860d52e2580dcd0db8ffb77b47992ed
Tina Eide <eide.tin@gmail.com>
2025-04-22 18:26:34 +0200

    Initial card details component

--- src/app/components/card-details/card-details.component.ts
+++ src/app/components/card-details/card-details.component.ts
@@ -1,10 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, inject, input, InputSignal, OnInit, signal, WritableSignal} from '@angular/core';
+import {CardDetailsService} from '../../services/card-details.service';
+import {GetCardDetails} from '../../models/card-details-models';
 
 @Component({
   selector: 'app-card-details',
   templateUrl: './card-details.component.html'
 })
 
-export class CardDetailsComponent {
+export class CardDetailsComponent implements OnInit {
 
+  cardId: InputSignal<number> = input.required<number>();
+
+  private cardDetailService: CardDetailsService = inject(CardDetailsService);
+
+  protected readonly cardDetails: WritableSignal<GetCardDetails> = signal<GetCardDetails>({} as GetCardDetails);
+
+  ngOnInit(): void {
+    this.getCardDetails();
+  }
+
+  getCardDetails(): void {
+    this.cardDetailService.getCardDetails(this.cardId()).subscribe({
+      next: (response: GetCardDetails): void => {
+        this.cardDetails.set(response);
+      },
+      error: (error: Error): void => {
+        console.error('Error fetching card details:', error.message);
+      }
+    });
+  }
 }
