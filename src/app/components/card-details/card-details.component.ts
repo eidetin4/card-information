27f9d47a01ff6c3970a693ad52f394536cad7e4b
Tina Eide <eide.tin@gmail.com>
2025-04-23 18:09:22 +0200

    Card as input as another request seems not necessary

--- src/app/components/card-details/card-details.component.ts
+++ src/app/components/card-details/card-details.component.ts
@@ -1,5 +1,4 @@
-import {Component, inject, input, InputSignal, OnInit, signal, WritableSignal} from '@angular/core';
-import {CardDetailsService} from '../../services/card-details.service';
+import {Component, input, InputSignal} from '@angular/core';
 import {GetCardDetails} from '../../models/card-details-models';
 import {DatePipe} from '@angular/common';
 
@@ -11,26 +10,7 @@ import {DatePipe} from '@angular/common';
   templateUrl: './card-details.component.html'
 })
 
-export class CardDetailsComponent implements OnInit {
+export class CardDetailsComponent {
 
-  cardId: InputSignal<number> = input.required<number>();
-
-  private cardDetailService: CardDetailsService = inject(CardDetailsService);
-
-  protected readonly cardDetails: WritableSignal<GetCardDetails> = signal<GetCardDetails>({} as GetCardDetails);
-
-  ngOnInit(): void {
-    this.getCardDetails();
-  }
-
-  getCardDetails(): void {
-    this.cardDetailService.getCard(this.cardId()).subscribe({
-      next: (response: GetCardDetails): void => {
-        this.cardDetails.set(response);
-      },
-      error: (error: Error): void => {
-        console.error('Error fetching card details:', error.message);
-      }
-    });
-  }
+  card: InputSignal<GetCardDetails> = input.required<GetCardDetails>();
 }
